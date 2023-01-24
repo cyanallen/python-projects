cc2dee07016197fa62f4ababeb7a9b0639add4d8
cyan allen <77130266+cyanallen@users.noreply.github.com>
2023-01-24 12:25:27 -0700

    Add files via upload

---
+++ mab-libs.py
@@ -0,0 +1,13 @@
+def mablib():
+
+    animal = input("Please enter a animal name: ")
+    color = input("Please enter in a color: ")
+    food = input("please enter in a food name: ")
+    name = input("please enter in a name: ")
+    place = input("Please enter in a place: ")
+
+    print("On this " + color + " day, was perfect to go to the " + place + 
+    " but on arrving there was a " + animal + " that got in the way and took the " 
+    + food + " out of " + name + " hands.") 
+
+mablib()
